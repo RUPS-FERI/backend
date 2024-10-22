21cf610915c5635969fc7852cd148dfc56a763c1
aljaz-kokol <52831109+aljaz-kokol@users.noreply.github.com>
2024-10-22 20:49:22 +0200

    chore: setup (#5)
    * setup: allow decorators
    
    * setup: initial http status codes
    
    * setup: initial error types
    
    * setup: file rename
    
    * setup: Error handling with new error types
    
    * setup: Error handling with new error types
    
    ---------
    
    Co-authored-by: ALjaz Kokol <aljaz.kokol@easy.bi>

--- src/_common/middlewares/error-handler.middleware.ts
+++ src/_common/middlewares/error-handler.middleware.ts
@@ -1,12 +1,15 @@
 import { type Request, type Response } from 'express';
+import { ApiError } from '../errors/index.js';
 
 export const globalErrorHandler = (
   error: Error,
   req: Request,
   res: Response,
 ) => {
-  res.json({
-    status: 500,
-    message: error.message,
+  const apiError = (error instanceof ApiError) ? error : ApiError.fromError(error);
+  res.status(apiError.status).json({
+    name: apiError.name,
+    message: apiError.message,
+    data: apiError.data,
   });
 };
---
+++ src/_common/utils/http/HttpStatusCode.ts
@@ -0,0 +1,8 @@
+export enum HttpStatusCode {
+  OK = 200,
+  CREATED = 201,
+  NOT_FOUND = 404,
+  FORBIDDEN = 403,
+  ALREADY_EXISTS = 409,
+  SERVER_ERROR = 500,
+}
