63b60b90d253327ce9b835b0de2a3a2251dff878
ALjaz Kokol <aljaz.kokol@easy.bi>
2024-10-23 00:17:54 +0200

    feature: Connect to database

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import dotenv from 'dotenv';
 
 import app from './app.js';
+import dataSource from '../db/typeorm.config.js';
 
 dotenv.config();
 
 const port = process.env.APP_PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`REST API running on port : ${port} (${process.env.ENV})`);
-});
+dataSource.initialize().then(() => {
+  console.log('Database Connected Successfully');
+
+  app.listen(port, () => {
+    console.log(`REST API running on port : ${port} (${process.env.ENV})`);
+  });
+})
+
+
